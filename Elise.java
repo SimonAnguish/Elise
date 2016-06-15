74a657d429515f9e04ce91b66d49c3fa9bb176da
Simon Anguish <simonanguish@mac.com>
2016-06-14 21:12:21 -0400

    Began transfering from CSV to sqlite

--- Elise.java
+++ Elise.java
@@ -1,8 +1,10 @@
 import java.io.*;
 import java.util.*;
+import java.sql.*;
 
 class Elise {
 	public static void main(String[] args) {
-		
+		Groceries g = new Groceries();
+//		g.add();
 	}
 }
--- Groceries.java
+++ Groceries.java
@@ -1,35 +1,30 @@
-import java.util.HashMap;
-import java.util.Scanner;
-import java.io.File;
-import java.io.PrintWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.*;
 
 class Groceries {
-	private String groceries_path = "./local-data/groceries.csv";
-	private File f = new File(groceries_path);
-	
-	private HashMap<String,Double> gl = new HashMap();
+	Connection c = null;
+	Statement stmt = null;
+	String sql;
 	
 	public Groceries() {
-		// Read the file
 		try {
-			Scanner scanner = new Scanner(f);
-			while (scanner.hasNext()) {
-				String next = scanner.nextLine();
-				gl.put(next.split(",")[0], Double.parseDouble(next.split(",")[1]));
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:kitchen.db");
+			c.setAutoCommit(false);
+			
+			stmt = c.createStatement();
+			sql = "SELECT name FROM sqlite_temp_master WHERE type='table' and name = 'GROCERIES';";
+			
+			if (stmt.executeUpdate(sql) == 0) {
+				sql = "CREATE TABLE GROCERIES (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT NOT NULL, COST REAL NOT NULL);";
+				stmt.executeQuery(sql);
+				stmt.close();
 			}
-		} catch (FileNotFoundException e) {
-			try {
-				f.createNewFile();
-			} catch(IOException e1) {
-				System.out.printf("IO Exception\n");
-			}
+		} catch (Exception e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
 		}
 	}
-	
-	public Groceries(File f) {}
 	
 	public void add() {
 		Scanner keyboard = new Scanner(System.in);
@@ -37,58 +32,69 @@ class Groceries {
 		String g = keyboard.nextLine();
 		System.out.printf("Price: $");
 		Double p = keyboard.nextDouble();
-		
-		gl.put(g, p);
+//			System.out.println(rs);
+
+//			sql = "INSERT INTO COMPANY (NAME, COST) " +
+//				"VALUES (?, ?);";
+//				
+//			PreparedStatement pstmt = c.prepareStatement(sql);
+//			pstmt.setString(1, g);
+//			pstmt.setDouble(2, p);
+//
+//			System.out.println(pstmt.executeQuery());
+//			pstmt.close();
+//			c.commit();
+//			
 	}
 	
-	public void list() {
-		for (String names: gl.keySet()) {
-			System.out.printf("%s, $%.2f\n", names, gl.get(names));
-		}
-	}
-	
-	public void remove() {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.printf("Remove: ");
-		String item = keyboard.nextLine();
-		remove(item);
-	}
-	
-	public void remove(String item) {
-		if (gl.containsKey(item)) {
-			gl.remove(item);
-		} else {
-			System.out.printf("%s is not known\n", item);
-		}
-	}
-	
-	public void update() {
-		try {
-			FileWriter write = new FileWriter(groceries_path, false);
-			PrintWriter print_line = new PrintWriter(write);
-		
-			for (String g: gl.keySet()) {
-				print_line.printf("%s,%f\n", g, gl.get(g));
-			}
-			
-			print_line.close();
-		} catch (IOException e) {
-			System.out.printf("Cannot open file");
-		}
-	}
-	
-	public void priceOf() {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.printf("Grocery: ");
-		String item = keyboard.nextLine();
-		priceOf(item);
-	}
-	
-	public void priceOf(String item) {
-		if (gl.containsKey(item)) {
-			System.out.printf("Price: $%.2f\n", gl.get(item));
-		} else {
-			System.out.printf("%s is not known\n", item);
-		}
-	}
+//	public void list() {
+//		for (String names: gl.keySet()) {
+//			System.out.printf("%s, $%.2f\n", names, gl.get(names));
+//		}
+//	}
+//	
+//	public void remove() {
+//		Scanner keyboard = new Scanner(System.in);
+//		System.out.printf("Remove: ");
+//		String item = keyboard.nextLine();
+//		remove(item);
+//	}
+//	
+//	public void remove(String item) {
+//		if (gl.containsKey(item)) {
+//			gl.remove(item);
+//		} else {
+//			System.out.printf("%s is not known\n", item);
+//		}
+//	}
+//	
+//	public void update() {
+//		try {
+//			FileWriter write = new FileWriter(groceries_path, false);
+//			PrintWriter print_line = new PrintWriter(write);
+//		
+//			for (String g: gl.keySet()) {
+//				print_line.printf("%s,%f\n", g, gl.get(g));
+//			}
+//			
+//			print_line.close();
+//		} catch (IOException e) {
+//			System.out.printf("Cannot open file");
+//		}
+//	}
+//	
+//	public void priceOf() {
+//		Scanner keyboard = new Scanner(System.in);
+//		System.out.printf("Grocery: ");
+//		String item = keyboard.nextLine();
+//		priceOf(item);
+//	}
+//	
+//	public void priceOf(String item) {
+//		if (gl.containsKey(item)) {
+//			System.out.printf("Price: $%.2f\n", gl.get(item));
+//		} else {
+//			System.out.printf("%s is not known\n", item);
+//		}
+//	}
 }
